ff10d136a3eaab2c8750cc4d715d6efd27ce9099
Raymond Chen <rchen1992@gmail.com>
2019-04-12 18:52:23 -0700

    Add LadderUser model.

---
+++ src/models/LadderUser.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize');
+const createModel = require('./createModel');
+const User = require('./User');
+const Ladder = require('./Ladder');
+
+module.exports = createModel('ladder_user', {
+    user_id: {
+        type: Sequelize.BIGINT,
+        primaryKey: true,
+        references: {
+            model: User,
+            key: 'user_id',
+        },
+    },
+    ladder_id: {
+        type: Sequelize.BIGINT,
+        primaryKey: true,
+        references: {
+            model: Ladder,
+            key: 'ladder_id',
+        },
+    },
+    rating: { type: Sequelize.INTEGER, allowNull: false },
+    rating_delta: { type: Sequelize.INTEGER, allowNull: false },
+});
