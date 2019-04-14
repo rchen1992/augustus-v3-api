fcdcb4fea9e3c83a810c7fc68ab2ce33ea02b6c3
Raymond Chen <rchen1992@gmail.com>
2019-04-13 18:09:37 -0700

    Get match by id query.

--- src/api/match/match.resolvers.js
+++ src/api/match/match.resolvers.js
@@ -2,9 +2,14 @@ const matches = (_, __, ctx) => {
     return ctx.models.match.findAll();
 };
 
+const match = (_, args, ctx) => {
+    return ctx.models.match.findByPk(args.id);
+};
+
 module.exports = {
     Query: {
         matches,
+        match,
     },
     Match: {
         id(match) {
