c66cee313b377a64f2128056e3b2249e666f5be3
Raymond Chen <rchen1992@gmail.com>
2019-04-17 22:40:18 -0700

    Get all matches test. Fix match mock data.

---
+++ src/tests/mockRepos/match.js
@@ -0,0 +1,11 @@
+const { mockMatches } = require('@tests/mockData/dataSet');
+
+module.exports = {
+    getAllMatches() {
+        return mockMatches;
+    },
+
+    getMatchById(matchId) {
+        return mockMatches.find(match => matchId === match.match_id);
+    },
+};
---
+++ src/tests/queries/getAllMatches.test.js
@@ -0,0 +1,25 @@
+const mockMatchesRepo = require('@tests/mockRepos/match');
+const { testQuery } = require('@tests/utils');
+
+testQuery({
+    id: 'Get all matches basic data',
+    query: `
+        {
+            matches {
+                id
+                tied
+                createdAt
+                updatedAt
+            }
+        }
+    `,
+    variables: {},
+    context: () => {
+        return {
+            repos: {
+                match: mockMatchesRepo,
+            },
+        };
+    },
+    expected: null,
+});
