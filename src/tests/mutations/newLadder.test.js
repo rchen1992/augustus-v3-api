4bcd060e34e3ab20b0e39ee160a66c4830ac1db6
Raymond Chen <rchen1992@gmail.com>
2019-04-25 21:55:52 -0700

    Create ladder mutation test.

---
+++ src/tests/mutations/newLadder.test.js
@@ -0,0 +1,49 @@
+const { testQuery } = require('@tests/utils');
+
+const userId = 1;
+const ladderName = 'My New Ladder';
+
+testQuery({
+    id: 'Create new ladder',
+    query: `
+        mutation createLadder($ladderName: String!, $userId: ID!) {
+            newLadder(ladderName: $ladderName, userId: $userId) {
+                id
+                ladderName
+                inviteToken
+                userRating
+                userRatingDelta
+            }
+        }
+    `,
+    variables: {
+        userId,
+        ladderName,
+    },
+    context: () => {
+        return {
+            services: {
+                ladder: {
+                    newLadder() {
+                        return {
+                            ladder_id: 10,
+                            ladder_name: ladderName,
+                            invite_token: 'tRJcyp2Jkr13',
+                            created_at: '2018-12-11 09:39:31',
+                            updated_at: '2018-10-09 01:00:11',
+                            ladder_user: {
+                                ladder_id: 10,
+                                user_id: userId,
+                                rating: 1000,
+                                rating_delta: 0,
+                                created_at: '2018-12-11 09:39:31',
+                                updated_at: '2018-10-09 01:00:11',
+                            },
+                        };
+                    },
+                },
+            },
+        };
+    },
+    expected: undefined,
+});
