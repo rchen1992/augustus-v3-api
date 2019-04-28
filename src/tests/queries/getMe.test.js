545d54a27fc131f8fa4c3b82a6417d83bad9bd55
Raymond Chen <rchen1992@gmail.com>
2019-04-28 15:46:00 -0700

    Get current user test.

---
+++ src/tests/queries/getMe.test.js
@@ -0,0 +1,35 @@
+const { testQuery } = require('@tests/utils');
+
+const currentUser = {
+    user_id: 10,
+    user_name: 'newboi',
+    email: 'newboi@gmail.com',
+    created_at: '2018-12-11 09:39:31',
+    updated_at: '2018-10-09 01:00:11',
+};
+
+testQuery({
+    id: 'Get current user',
+    query: `
+        {
+            me {
+                id
+                userName
+                email
+                createdAt
+            }
+        }
+    `,
+    variables: {},
+    context: () => {
+        return {
+            currentUser,
+            req: {
+                session: {
+                    userId: currentUser.user_id,
+                },
+            },
+        };
+    },
+    expected: undefined,
+});
