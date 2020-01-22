218f934de2cf7bfcc856ecbb035ed583e35bb497
Raymond Chen <rchen1992@gmail.com>
2020-01-21 20:33:35 -0800

    Add more comments to syncUser.

--- src/middleware/syncUser.js
+++ src/middleware/syncUser.js
@@ -2,12 +2,29 @@ const createLoaders = require('@loaders');
 const createUserRepo = require('@repos/userRepo');
 const createUserService = require('@services/userService');
 
+/**
+ * Middleware that syncs users from Auth0 to our own DB.
+ * Why would we want to manage our own user table when Auth0 already has one?
+ * Two main reasons:
+ * - add additional fields to a user object (ex. unique username)
+ * - perform table joins with the user table instead of making calls
+ *  to Auth0's REST API and manually joining in memory.
+ */
 const syncUser = async (req, res, next) => {
     console.log('req user', req.user);
     console.log('req user data', JSON.parse(req.headers.userdata));
 
+    /**
+     * `req.user` is set by the JWT auth middleware.
+     */
     const userId = req.user.sub;
+
+    /**
+     * The frontend sends stringified JSON in a header for the user data.
+     * This is required in order to add the user data to our DB.
+     */
     const userData = req.headers.userdata ? JSON.parse(req.headers.userdata) : null;
+
     if (!userId || !userData) {
         return next();
     }
